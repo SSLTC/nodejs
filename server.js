5fe06202038fdec3b48afd70dbdb7d09c88d711c
Sven Snoeck <snoeck.sven@live.be>
2023-02-14 21:12:56 +0100

    http server

--- server.js
+++ server.js
@@ -1,147 +1,54 @@
+const http = require("http");
 const path = require("path");
 const fs = require("fs");
 
-/*
+function send404response(response, err) {
+  response.setHeader("Content-Type", "text/html");
+  response.writeHead(404);
+  response.write("ERROR 404: ");
+  response.end(err.message);
+}
 
-console.log("This is a console log from my server.js file");
+const server = http.createServer();
 
-console.log("Hello Wold");
+server.on("request", (request, response) => {
+  console.log(`A request has been submitted to ${request.url}`);
+  if (request.method == "GET") {
+    const contentType = request.headers.accept.split(",")[0];
+    let filePath = `./client${request.url}`;
 
-// Base file name
-console.log(path.basename(__filename));
+    if (contentType == "text/html") {
+      filePath = `./client${request.url}` + "/index.html";
+    }
+    if (contentType == "text/css") {
+      let subFolder = path.basename(request.headers.referer);
+      console.log(subFolder);
+      console.log(path.resolve(subFolder));
+      if (subFolder != "") {
+        subFolder = "/" + subFolder;
+      }
+      filePath = `./client${subFolder}${request.url}`;
+      console.log(filePath);
+    }
 
-// Directory name
-console.log(path.dirname(__filename));
+    const stream = fs.createReadStream(filePath);
+    stream.once("readable", () => {
+      // If the stream becomes readable, then set the Content-Type header and a 200 OK status.
+      // Then pipe the file reader stream to the response. The pipe will automatically call response.end() when the stream ends.
+      response.setHeader("Content-Type", contentType);
 
-// File extension
-console.log(path.extname(__filename));
+      response.writeHead(200);
+      stream.pipe(response);
+      // .pipe() feeds a readable stream into a writable stream so that as the contents are incrementally read from the readable stream,
+      // they are automatically forwarded to the writable stream. It manages the reading from one stream and writing to another stream automatically for you.
+    });
 
-// Create path object
-console.log(path.parse(__filename));
-// Get property from path object
-console.log(path.parse(__filename).base);
-
-// Concatenate paths
-console.log(path.join(__dirname, "test", "hello.html"));
-
-*/
-
-const folderExists = (folder, create = false) => {
-  const folderExists = fs.existsSync(path.join(__dirname, `/${folder}`));
-  if (!folderExists && create) {
-    createFolder(folder);
+    stream.on("error", (err) => {
+      send404response(response, err);
+    });
   }
-  return folderExists;
-};
+});
 
-const fileExists = (folder, file) => {
-  return fs.existsSync(path.join(__dirname, `/${folder}`, file));
-};
-
-const createFolder = (folder) => {
-  fs.mkdir(path.join(__dirname, `/${folder}`), {}, (err) => {
-    if (err) throw err;
-    console.log(`Folder ${folder} created...`);
-  });
-};
-
-const createFile = ({ folder, file, text }) => {
-  fs.writeFile(path.join(__dirname, `/${folder}`, file), text, (err) => {
-    if (err) throw err;
-    console.log(`File ${file} created...`);
-  });
-};
-
-const appendFile = ({ folder, file, text }) => {
-  fs.appendFile(path.join(__dirname, `/${folder}`, file), text, (err) => {
-    if (err) throw err;
-    console.log(`File ${file} written to...`);
-  });
-};
-
-const readFile = (folder, file) => {
-  fs.readFile(path.join(__dirname, `/${folder}`, file), "utf8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-};
-
-const renameFile = ({ folder, oldFile, newFile }) => {
-  fs.rename(
-    path.join(__dirname, `/${folder}`, oldFile),
-    path.join(__dirname, `/${folder}`, newFile),
-    (err) => {
-      if (err) throw err;
-      console.log(`File ${oldFile} renamed to ${newFile}`);
-    }
-  );
-};
-
-const createHtmlFile = (folder, title) => {
-  const content = `<html lang='en'><head><meta charset='UTF-8' /><meta http-equiv='X-UA-Compatible' content='IE=edge' /><meta name='viewport' content='width=device-width, initial-scale=1.0' /><link rel='stylesheet' href='./style.css' /><title>${title}</title></head><body><h1>${title}</h1></body></html>`;
-  createFile({ folder: folder, file: "index.html", text: content });
-};
-
-const createCssFile = (folder, color) => {
-  const content = `body { background-color: ${color};}`;
-  createFile({ folder: folder, file: "style.css", text: content });
-};
-
-const removeFolders = (folder = "client") => {
-  try {
-    fs.rmdirSync(path.join(__dirname, `/${folder}`), { recursive: true });
-
-    console.log(`${folder} is deleted!`);
-  } catch (err) {
-    console.error(`Error while deleting ${folder}.`);
-  }
-};
-
-removeFolders();
-
-const createFolders = () => {
-  const folderStructure = [
-    "client",
-    "client/contact",
-    "client/about",
-    "client/blog",
-  ];
-
-  folderStructure.forEach((value) => {
-    folderExists(value, true);
-  });
-};
-
-const createFiles = () => {
-  const fileStructure = [
-    {
-      folder: "client",
-      title: "Home",
-      color: "808080",
-    },
-    {
-      folder: "client/contact",
-      title: "Contact",
-      color: "ff0000",
-    },
-    {
-      folder: "client/about",
-      title: "About",
-      color: "00ff00",
-    },
-    {
-      folder: "client/blog",
-      title: "Blog",
-      color: "0000ff",
-    },
-  ];
-
-  fileStructure.forEach((value) => {
-    if (!fileExists(value.folder, "style.css")) {
-      createCssFile(value.folder, value.color);
-    }
-    if (!fileExists(value.folder, "index.html")) {
-      createHtmlFile(value.folder, value.title);
-    }
-  });
-};
+server.listen(3000, () => {
+  console.log("Server started on http://127.0.0.1:3000");
+});
